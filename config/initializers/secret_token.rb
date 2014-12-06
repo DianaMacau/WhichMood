a15a3a46f0b30a49a767ad3254ac6b2bf73baf3a
Diana <diana.macau@gmail.com>
2014-12-06 17:36:52 +0200

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhichMood::Application.config.secret_token = 'cf85a7fc2ccb3230fe6d30f7f750cb20897f0f0fc895b99722d5e628d66a702c845e104436c4392d04abf67468931233bd8665df3d868e7a34580f60814b9e5e'
